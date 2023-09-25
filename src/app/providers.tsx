f2ab78a80b29d28047d550c0fcda7dacf76080d4
taxio <taxio.2029@gmail.com>
2023-09-25 12:01:26 +0900

    setup ChakraUI

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import React from "react";
+
 import {UserProvider} from "@auth0/nextjs-auth0/client";
+import {ChakraProvider} from "@chakra-ui/react";
+import {CacheProvider} from "@chakra-ui/next-js";
+
+import {theme} from "@/styles";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <UserProvider>
-      {children}
-    </UserProvider>
+    <CacheProvider>
+      <ChakraProvider theme={theme}>
+        <UserProvider>
+          {children}
+        </UserProvider>
+      </ChakraProvider>
+    </CacheProvider>
   )
 }
